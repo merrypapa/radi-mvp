f1b54e96ba7f03ae39391e6e71bb30a38613683f
Eric Kang <ekang@hqc.com>
2022-08-14 22:31:45 -0700

    typo update suite -> suit

--- radi-mvp.js
+++ radi-mvp.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $.get("http://deckofcardsapi.com/api/deck/new/draw/?count=1", function(data){
         const suit = data.cards[0].suit;
-        console.log(suite);
+        console.log(suit);
     });
 });
